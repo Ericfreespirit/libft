30b5a19c1972e892872a006f7de5cff7e7c61415
Eric LING <eriling@e1r8p14.42.fr>
2021-02-08 09:36:50 +0100

    add ft_atof_2.c

--- ft_atof_2.c
+++ ft_atof_2.c
@@ -1,47 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atof.c                                          :+:      :+:    :+:   */
+/*   ft_atof_2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 14:16:51 by eriling           #+#    #+#             */
-/*   Updated: 2021/02/08 09:35:56 by eriling          ###   ########.fr       */
+/*   Updated: 2021/02/08 09:36:21 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-double	solve_ft_atof(char *str, double inter_part, double fract_part, int div)
+double	solve_ft_atof_2(char **str, double inter_part, double fract_part, int div)
 {
 	int	is_fract;
 
 	is_fract = 0;
-	while (*str)
+	while (**str)
 	{
-		if (*str >= '0' && *str <= '9')
+		if (**str >= '0' && **str <= '9')
 		{
 			if (is_fract)
 			{
-				fract_part = fract_part * 10 + (*str - '0');
+				fract_part = fract_part * 10 + (**str - '0');
 				div *= 10;
 			}
 			else
-				inter_part = inter_part * 10 + (*str - '0');
+				inter_part = inter_part * 10 + (**str - '0');
 		}
-		else if (*str == '.')
+		else if (**str == '.')
 		{
 			if (is_fract)
 				return (inter_part + fract_part / div);
 			else
 				is_fract = 1;
 		}
-		str++;
+		(*str)++;
 	}
 	return (inter_part + fract_part / div);
 }
 
-double	ft_atof(char *str)
+double	ft_atof_2(char **str)
 {
 	int		sign;
 	double	inter_part;
@@ -54,13 +54,13 @@ double	ft_atof(char *str)
 	inter_part = 0;
 	fract_part = 0;
 	sign = 1;
-	while ((*str >= 9 && *str <= 13) || *str == ' ')
-		str++;
-	if (*str == '+' || *str == '-')
+	while ((**str >= 9 && **str <= 13) || **str == ' ')
+		(*str)++;
+	if (**str == '+' || **str == '-')
 	{
-		if (*str == '-')
+		if (**str == '-')
 			sign *= -1;
-		str++;
+		(*str)++;
 	}	
-	return (sign * solve_ft_atof(str, inter_part, fract_part, div));
+	return (sign * solve_ft_atof_2(str, inter_part, fract_part, div));
 }
