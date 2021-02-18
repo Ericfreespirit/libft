0ea0e27cd47437f75d1134611f445a32526566fb
Eric LING <eriling@e1r8p10.42.fr>
2021-02-18 12:17:29 +0100

    norme ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,11 +6,36 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 16:02:03 by eriling           #+#    #+#             */
-/*   Updated: 2020/10/28 17:45:50 by eriling          ###   ########.fr       */
+/*   Updated: 2021/02/18 12:17:08 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	init_ft_itoa(long *val, int *i, int *sign, int n)
+{
+	if (n < 0)
+	{
+		*val = - (long)n;
+		*i = 2;
+		*sign = -1;
+	}
+	else
+	{
+		*val = n;
+		*i = 1;
+		*sign = 0;
+	}
+}
+
+void	*len_ft_itoa(long val, int i)
+{
+	while (val > 0)
+	{
+		val /= 10;
+		i++;
+	}
+}
 
 char	*ft_itoa(int n)
 {
@@ -19,20 +44,22 @@ char	*ft_itoa(int n)
 	int		sign;
 	char	*s;
 
-	val = (n < 0) ? -(long)n : n;
-	i = (n < 0) ? 2 : 1;
-	sign = (n < 0) ? -1 : 0;
-	while (val /= 10)
-		i++;
-	if (!(s = malloc(sizeof(*s) * i + 1)))
+	init_ft_itoa(&val, &i, &sign, n);
+	len_ft_itoa(val, i);
+	s = malloc(sizeof(*s) * i + 1);
+	if (!s)
 		return (NULL);
 	s[i] = '\0';
-	val = (n < 0) ? -(long)n : n;
+	if (n < 0)
+		val = - (long)n;
+	else
+		val = n;
 	while (i-- + sign)
 	{
 		s[i] = (val % 10) + '0';
 		val /= 10;
 	}
-	sign == -1 ? s[0] = '-' : 0;
+	if (sign == -1)
+		s[0] = '-';
 	return (s);
 }
